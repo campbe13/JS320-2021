fc99b05638efb51308658b8e745e84049fc9c0e7
tricia <pcampbell.edu@gmail.com>
2021-09-28 12:57:10 -0400

    add skel

---
+++ 06-JSON-Map/skel/site.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * description here
+ * @author your name
+ */
+document.addEventListener("DOMContentLoaded", setup)
+
+function setup () {
+    // setup code
+}
+
+// your functions
